cdd81ad25927ea12f00171576496bb41e02bb950
Beri28 <cletusberinyuy@gmail.com>
2025-03-26 10:39:38 +0100

    second image

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+//   const {currentUser}=useAuth()
+  return (
+    <>
+      <p>Hello</p>
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+
+import { createRoot } from 'react-dom/client'
+import React from 'react'
+import App from './App'
+import "./index.css"
+import { BrowserRouter } from 'react-router-dom'
+// import AuthProvider from './context/authContext'
+createRoot(document.getElementById('root')).render(
+    // <AuthProvider>
+    //     <BrowserRouter>
+            <App />
+    //     </BrowserRouter>
+    // </AuthProvider>
+  
+)
